35a1613504e4be3aae2cd110fe189abf2a71d8a0
Kiril Anastasoff <kirilanastasoff@gmail.com>
2022-08-25 09:36:04 +0200

    In memory user details

--- Spring-Security/Spring-Security/src/main/java/com/kirilanastasoff/springsecurity/SpringSecurity/security/ApplicationSecurityConfig.java
+++ Spring-Security/Spring-Security/src/main/java/com/kirilanastasoff/springsecurity/SpringSecurity/security/ApplicationSecurityConfig.java
@@ -1,26 +1,36 @@
 package com.kirilanastasoff.springsecurity.SpringSecurity.security;
 
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @SuppressWarnings("deprecation")
 @Configurable
 @EnableWebSecurity
-public class ApplicationSecurityConfig  extends WebSecurityConfigurerAdapter{
-	
-	
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-		.authorizeRequests()
-		.antMatchers("/", "index","/css/*", "/js/*")
-		.permitAll()
-		.anyRequest()
-		.authenticated()
-		.and()
-		.httpBasic();
-	}
-	
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/", "index", "/css/*", "/js/*").permitAll().anyRequest().authenticated()
+				.and().httpBasic();
+	}
+
+	@Override
+	@Bean
+	protected UserDetailsService userDetailsService() {
+		UserDetails ivanov = 
+				User.builder()
+				.username("ivanovivan")
+				.password("password")
+				.roles("student")
+				.build();
+
+		return new InMemoryUserDetailsManager(ivanov);
+	}
 }
