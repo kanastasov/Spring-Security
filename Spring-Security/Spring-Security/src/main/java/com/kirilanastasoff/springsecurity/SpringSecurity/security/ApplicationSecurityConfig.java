ddae796520078340e01d7a65406b760a45b61128
Kiril Anastasoff <kirilanastasoff@gmail.com>
2022-08-26 09:55:52 +0200

    redirest after successfull login

--- Spring-Security/Spring-Security/src/main/java/com/kirilanastasoff/springsecurity/SpringSecurity/security/ApplicationSecurityConfig.java
+++ Spring-Security/Spring-Security/src/main/java/com/kirilanastasoff/springsecurity/SpringSecurity/security/ApplicationSecurityConfig.java
@@ -46,7 +46,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 				.and()
 //				.httpBasic();
 				.formLogin()
-				.loginPage("/login").permitAll();
+				.loginPage("/login").permitAll()
+				.defaultSuccessUrl("/courses", true);
 	}
 
 	@Override
