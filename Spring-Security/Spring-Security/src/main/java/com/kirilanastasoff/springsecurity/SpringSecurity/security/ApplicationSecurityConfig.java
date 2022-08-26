9771a138745072e3f097c7be01d1f52a2d9bcb6c
Kiril Anastasoff <kirilanastasoff@gmail.com>
2022-08-26 09:28:32 +0200

    starting with form validation

--- Spring-Security/Spring-Security/src/main/java/com/kirilanastasoff/springsecurity/SpringSecurity/security/ApplicationSecurityConfig.java
+++ Spring-Security/Spring-Security/src/main/java/com/kirilanastasoff/springsecurity/SpringSecurity/security/ApplicationSecurityConfig.java
@@ -35,15 +35,17 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-		.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-		.and()
+//		.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//		.and()
+		.csrf().disable()
 		.authorizeRequests()
 		.antMatchers("/", "index", "/css/*", "/js/*").permitAll()
 		.antMatchers("/api/**").hasRole(ApplicationUserRole.STUDENT.name())
 		.anyRequest()
 		.authenticated()
 				.and()
-				.httpBasic();
+//				.httpBasic();
+				.formLogin();
 	}
 
 	@Override
