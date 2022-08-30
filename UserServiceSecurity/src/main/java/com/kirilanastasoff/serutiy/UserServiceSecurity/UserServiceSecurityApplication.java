df5cda8835dc4e4d72af08e7bae8a116179a3f7c
Kiril Anastasoff <kirilanastasoff@gmail.com>
2022-08-30 09:22:19 +0200

    Initial project commit

---
+++ UserServiceSecurity/src/main/java/com/kirilanastasoff/serutiy/UserServiceSecurity/UserServiceSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.kirilanastasoff.serutiy.UserServiceSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceSecurityApplication.class, args);
+	}
+
+}
---
+++ UserServiceSecurity/src/test/java/com/kirilanastasoff/serutiy/UserServiceSecurity/UserServiceSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kirilanastasoff.serutiy.UserServiceSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
