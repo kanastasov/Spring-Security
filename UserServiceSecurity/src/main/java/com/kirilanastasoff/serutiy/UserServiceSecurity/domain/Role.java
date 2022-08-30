ccb43e5d6c429459b9508b5e429f4a37011d4ed1
Kiril Anastasoff <kirilanastasoff@gmail.com>
2022-08-30 09:39:23 +0200

    add the model

---
+++ UserServiceSecurity/src/main/java/com/kirilanastasoff/serutiy/UserServiceSecurity/domain/Role.java
@@ -0,0 +1,22 @@
+package com.kirilanastasoff.serutiy.UserServiceSecurity.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String name;
+
+}
