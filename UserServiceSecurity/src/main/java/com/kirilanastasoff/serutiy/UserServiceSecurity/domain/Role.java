95a6cfacda90dedb320a65f8caaa8f1689ca69fb
Kiril Anastasoff <kirilanastasoff@gmail.com>
2022-08-31 10:05:12 +0200

    fixed error with user default table

--- UserServiceSecurity/src/main/java/com/kirilanastasoff/serutiy/UserServiceSecurity/domain/Role.java
+++ UserServiceSecurity/src/main/java/com/kirilanastasoff/serutiy/UserServiceSecurity/domain/Role.java
@@ -3,6 +3,7 @@ package com.kirilanastasoff.serutiy.UserServiceSecurity.domain;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -10,19 +11,24 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "role")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Role {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(name = "name")
 	private String name;
 	public Long getId() {
 		return id;
@@ -37,9 +43,13 @@ public class Role {
 		this.name = name;
 	}
 
-	@ManyToMany(fetch = FetchType.EAGER)
-    @JoinColumn(name = "user_id")
-	private Collection<User> users = new ArrayList<>();
+//	@ManyToMany(fetch = FetchType.EAGER)
+//    @JoinColumn(name = "id")
+//	private Collection<User> users = new ArrayList<>();
+	
+//	@ManyToOne(fetch = FetchType.EAGER)
+//    @JoinColumn(name = "user_id", referencedColumnName = "id")
+//    private User users;
 	
 	
 }
--- UserServiceSecurity/src/main/java/com/kirilanastasoff/serutiy/UserServiceSecurity/domain/User.java
+++ UserServiceSecurity/src/main/java/com/kirilanastasoff/serutiy/UserServiceSecurity/domain/User.java
@@ -2,7 +2,10 @@ package com.kirilanastasoff.serutiy.UserServiceSecurity.domain;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -10,29 +13,53 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "users")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name = "name")
 	private String name;
+	
+	@Column(name = "username")
 	private String username;
+	
+	@Column(name = "password")
 	private String password;
 
-//	@ManyToMany(fetch = FetchType.EAGER)
-	@ManyToMany(fetch = FetchType.LAZY)
-    @JoinColumn(name = "role_id")
+	@ManyToMany(fetch = FetchType.EAGER)
+//	@ManyToMany(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "role_id")
 	private Collection<Role> roles = new ArrayList<>();
 
+	
+	
+//	@LazyCollection(LazyCollectionOption.FALSE)
+//	@JsonManagedReference
+//	@OneToMany(mappedBy = "users", orphanRemoval = true, cascade = { CascadeType.MERGE, CascadeType.DETACH,
+//			CascadeType.PERSIST, CascadeType.REMOVE })
+//	private List<Role> roles = new ArrayList<>();
+	
+	
+	
 	public Long getId() {
 		return id;
 	}
